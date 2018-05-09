58bfd5f4bb63434060b2c68368fed3ce1ffcc012
104974 <104974@SALA408.proway.treina>
2018-05-09 19:58:47 -0300

    Exercicio 2 completo

---
+++ src/exercicio2/Ex02.java
@@ -0,0 +1,94 @@
+package exercicio2;
+
+import javax.swing.JOptionPane;
+
+public class Ex02 {
+
+	String nome, nomemaioraltura="",nomemaioridade="",nomemaiorpeso="";
+	int idade,qtdJogadores=0 ;
+	double peso,altura,maiorPeso,maiorAltura,maiorIdade,somaAltura=0;
+	
+	
+	
+	public void perguntas(){
+		 
+		nome = JOptionPane.showInputDialog("Informe nome do jogador");
+		qtdJogadores++;
+		if(!nome.equals("sair")){
+			idade = Integer.parseInt(JOptionPane.showInputDialog("Idade : "));			
+			peso = Double.parseDouble(JOptionPane.showInputDialog("Peso : "));			
+			altura = Double.parseDouble(JOptionPane.showInputDialog("Altura"));
+		}
+	}
+	public void laco(){
+		
+		int continuar = 0;
+		
+		do{
+			perguntas();
+			resultados();
+			mediaAltura();
+			
+			continuar = JOptionPane.showConfirmDialog(null,"Deseja continuar a cadastrar ? ","", 0);
+			
+		}while(continuar == 0);
+		exibir();
+		
+	}
+	
+	public void resultados(){
+		
+				
+		if(idade > maiorIdade){
+			maiorIdade = idade;
+			nomemaioridade = nome;
+		}
+		
+		if(peso > maiorPeso){
+			maiorPeso = peso;
+			nomemaiorpeso = nome;
+		}
+		
+		
+		somaAltura += altura;
+		if(altura > maiorAltura){
+			maiorAltura = altura;
+			nomemaioraltura = nome;
+		}
+	}
+
+	public double mediaAltura(){
+		
+		double media = somaAltura / qtdJogadores;
+			
+		return media;
+		
+	}
+	public void exibir(){
+		
+		String msg = "***** Informacoes *****\n\n";
+			   msg+= "Quantidade Jogadores " +qtdJogadores;
+			   msg+= "\nMaior jogador: " +nomemaioraltura;
+			   msg+= "\nJogador mais velho: " +nomemaioridade;
+			   msg+= "\nJogador mais pesado: " +nomemaiorpeso;
+			   msg+= "\nMedia de alturas "+String.format("%.2f", mediaAltura());
+			   
+			   JOptionPane.showMessageDialog(null, msg);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/exercicio2/Principal.java
@@ -0,0 +1,11 @@
+package exercicio2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Ex02 e = new Ex02();
+		e.laco();
+
+	}
+
+}
